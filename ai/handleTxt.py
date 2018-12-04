96cbfa40d291ddc59ca0c8232572c7ef4e7bc134
ter <a26214311@gmail.com>
2018-12-04 11:42:17 +0800

    txt

--- ai/handleTxt.py
+++ ai/handleTxt.py
@@ -1,9 +1,10 @@
 import weather
 
 def handletxt(txt):
-    ret = ""
     if txt.endswith(u"天气") and len(txt)<8 :
         ret = weather.getWeather(txt)
     elif txt.index(u"行情")>0:
+        ret = ""
+    else:
         ret = ""
     return ret
